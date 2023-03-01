f5d16268f69b291c3be9396af5639a9af6b9ab6f
Gowtham.N <gowtham.n@qualitestgroup.com>
2023-03-01 17:30:22 +0530

    fcvbnjk

--- src/test/java/TestRunner/AllRunner.java
+++ src/test/java/TestRunner/AllRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features="src/test/feature/mainScenario.feature",
 		glue="StepsDefination",
-		tags="@tag3",
+		tags="@tag2",
 		monochrome=true,
 	    plugin={"pretty","io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm","json:target/cucumber.json"}
 		)
