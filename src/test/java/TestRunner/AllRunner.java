32649ad58aa8035b50b78dbf25f345b6152132c9
Gowtham.N <gowtham.n@qualitestgroup.com>
2023-03-03 11:14:07 +0530

    Changes is done....

--- src/test/java/TestRunner/AllRunner.java
+++ src/test/java/TestRunner/AllRunner.java
@@ -8,8 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features="src/test/feature/mainScenario.feature",
 		glue="StepsDefination",
-		tags="@tag1",
-		monochrome=true,
+		tags="@tag1 or @tag2",
+		monochrome=true, 
 	    plugin={"pretty","io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm","json:target/cucumber.json"}
 		)
 public class AllRunner extends AbstractTestNGCucumberTests{
